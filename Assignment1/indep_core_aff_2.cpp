6c787d0d686928b4a4b9710d68fbf84c7a7f5225
sapi <sapi@itu.dk>
2025-03-18 09:30:28 +0100

    getting experiment 2 ready

--- Assignment1/indep_core_aff_2.cpp
+++ Assignment1/indep_core_aff_2.cpp
@@ -21,11 +21,9 @@ void partitionInput(int numThread, int start, int end, int numPartitions, vector
         tuple<int64_t, int64_t> t = input[i];
         int partitionKey = hashFunction(get<0>(t), numPartitions);
         partitions[partitionKey].push_back(t);
-        // std::cout << "Thread #" << i << ": on CPU " 
-        //         << sched_getcpu() << "\n"; // same thing as independent
     }
-    std::cout << "Thread #" << numThread << ": on CPU " 
-                << sched_getcpu() << "\n"; 
+    // std::cout << "Thread #" << numThread << ": on CPU " 
+    //             << sched_getcpu() << "\n"; 
 }
 
 void cleanup(std::vector<std::vector<Partition>>& threadPartitions) {
@@ -57,14 +55,13 @@ int main(int argc, char* argv[]) {
         for (auto& partition : threadPartition)
             partition.reserve(sizePartition);  
 
-    int numCores = atoi(argv[3]);
-    cpu_set_t cpuset[numCores];
-    for(int i=0; i < numCores; i++) {
+    cpu_set_t cpuset[numThreads];
+    for(int i=0; i < numThreads; i++) {
         // Clear it and mark only CPU i as set.
         CPU_ZERO(&cpuset[i]);
         //loop thru the args given to compute cpu id
-        CPU_SET(atoi(argv[4 + i]), &cpuset[i]);
-        // CPU_SET(i*2, &cpuset[i]);
+        // CPU_SET(atoi(argv[3 + i]), &cpuset[i]);
+        CPU_SET(i*2, &cpuset[i]);
     }
 
     auto start_clock = std::chrono::steady_clock::now();
