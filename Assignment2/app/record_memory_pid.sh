d4101a6477de55bb1d5e02a5b2de32327ec40c58
sapi <sapi@itu.dk>
2025-05-02 14:55:32 +0200

    test test test

--- Assignment2/app/record_memory_pid.sh
+++ Assignment2/app/record_memory_pid.sh
@@ -16,8 +16,14 @@ fi
 shift 2  
 PROGRAM_ARGS=$(printf "%s," "$@")
 PROGRAM_ARGS=${PROGRAM_ARGS%,}
+
 while kill -0 "$PID" 2> /dev/null; do
-  top -b -n 1 -p "$PID" | awk -v args="$PROGRAM_ARGS" \
-    '/^[[:space:]]*'"$PID"'/ {print args "," $5 "," $6 "," $7}' >> "$OUTPUT_FILE"
-  sleep 3
+  # top -b -n 1 -p "$PID" | awk -v args="$PROGRAM_ARGS" \
+  #   '/^[[:space:]]*'"$PID"'/ {print args "," $5 "," $6 "," $7}' >> "$OUTPUT_FILE"
+  # sleep 3
+    if [ -f /proc/$PID/status ]; then
+        MEM_KB=$(grep VmRSS /proc/$PID/status | awk '{ print $2 }')
+        echo "$PROGRAM_ARGS, $MEM_KB" >> "$OUTPUT_FILE"
+    fi
+    sleep 1
 done
