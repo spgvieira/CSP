d70a5c0254334a139612879e4f958a40ff7c7aa4
sapi <sapi@itu.dk>
2025-05-07 12:02:36 +0200

    adding baseline task

--- Assignment2/app/memory.sh
+++ Assignment2/app/memory.sh
@@ -13,24 +13,19 @@ PROGRAM_ARGS=$(printf "%s," "$@")
 PROGRAM_ARGS=${PROGRAM_ARGS%,}
 
 # echo "timestamp,program_args,total_mem_mib,pid_mem_kb" >> "$OUTPUT_FILE"
+if [[ "$PID" != "baseline" ]]; then
+    while kill -0 "$PID" 2> /dev/null; do
+        FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+        PID_MEM_KB=$(grep VmRSS /proc/$PID/status | awk '{ print $2 }')
+        echo "$PROGRAM_ARGS,$FREE_MEM,$PID_MEM_KB" >> "$OUTPUT_FILE"
+        sleep 1
+    done
+else 
+    END_TIME=$(date -d "now + 3 minutes" +%s)
 
-while kill -0 "$PID" 2> /dev/null; do
-    # TIMESTAMP=$(date +%Y-%m-%d_%H:%M:%S)
-
-    # Get total memory usage (MiB)
-    TOTAL_MEM=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
-    # TOTAL_MEM_MIB=$(grep MemTotal "$TOTAL_MEM_INFO" | awk '{print $2 / 1024}')
-    FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
-    # FREE_MEM_MIB=$(grep MemFree "$TOTAL_MEM_INFO" | awk '{print $2 / 1024}')
-    # USED_MEM=$(echo "$TOTAL_MEM - $FREE_MEM")
-
-  # Get PID memory usage (KB)
-#   if [ -f /proc/$PID/status ]; then
-    PID_MEM_KB=$(grep VmRSS /proc/$PID/status | awk '{ print $2 }')
-#   else
-#     PID_MEM_KB="N/A"
-#   fi
-
-    echo "$PROGRAM_ARGS,$TOTAL_MEM,$FREE_MEM,$PID_MEM_KB" >> "$OUTPUT_FILE"
-    sleep 1
-done
+    while [ $(date +%s) -lt "$END_TIME" ]; do
+        FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+        echo "$FREE_MEM" >> "$OUTPUT_FILE"
+        sleep 1
+    done
+fi
