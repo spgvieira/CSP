ff91480afaa8e83c22e95dedc82de8e5193168f7
sapi <sapi@itu.dk>
2025-05-14 17:34:40 +0200

    baseline script fix due to weird space interpretation on server

---
+++ Assignment2/app/baseline_memory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUTPUT_FILE="$1"
+
+END_TIME=$(date -d "now + 1 minutes" +%s)
+
+while [ $(date +%s) -lt "$END_TIME" ]; do
+    FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+    echo "$FREE_MEM" >> "$OUTPUT_FILE"
+    sleep 1
+done
--- Assignment2/app/memory.sh
+++ Assignment2/app/memory.sh
@@ -13,19 +13,9 @@ PROGRAM_ARGS=$(printf "%s," "$@")
 PROGRAM_ARGS=${PROGRAM_ARGS%,}
 
 # echo "timestamp,program_args,total_mem_mib,pid_mem_kb" >> "$OUTPUT_FILE"
-if [[ "$PID" != "baseline" ]]; then
-    while kill -0 "$PID" 2> /dev/null; do
-        FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
-        PID_MEM_KB=$(grep VmRSS /proc/$PID/status | awk '{ print $2 }')
-        echo "$PROGRAM_ARGS,$FREE_MEM,$PID_MEM_KB" >> "$OUTPUT_FILE"
-        sleep 1
-    done
-else 
-    END_TIME=$(date -d "now + 3 minutes" +%s)
-
-    while [ $(date +%s) -lt "$END_TIME" ]; do
-        FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
-        echo "$FREE_MEM" >> "$OUTPUT_FILE"
-        sleep 1
-    done
-fi
+while kill -0 "$PID" 2> /dev/null; do
+    FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+    PID_MEM_KB=$(grep VmRSS /proc/$PID/status | awk '{ print $2 }')
+    echo "$PROGRAM_ARGS,$FREE_MEM,$PID_MEM_KB" >> "$OUTPUT_FILE"
+    sleep 1
+done 
