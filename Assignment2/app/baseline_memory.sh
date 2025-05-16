9d497595a71aa67c5d63eb02f47eceb369c89124
Soplight <sophuskm@hotmail.dk>
2025-05-16 15:41:23 +0200

    baseline attempt

--- Assignment2/app/baseline_memory.sh
+++ Assignment2/app/baseline_memory.sh
@@ -2,7 +2,7 @@
 
 OUTPUT_FILE="$1"
 
-END_TIME=$(date -d "now + 1 minutes" +%s)
+END_TIME=$(date -d "now + 3 minutes" +%s)
 
 while [ $(date +%s) -lt "$END_TIME" ]; do
     FREE_MEM=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
